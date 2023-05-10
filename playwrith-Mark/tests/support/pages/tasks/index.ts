f4c492751ede08885e025b3f099d1653b1774131
Marllon Menezes <marllon_dallas@hotmail.com>
2023-05-09 23:13:22 -0300

    inclusão de page objects

--- playwrith-Mark/tests/support/pages/tasks/index.ts
+++ playwrith-Mark/tests/support/pages/tasks/index.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { Page, expect } from '@playwright/test';
 import { TaskModel } from '../../../fixtures/task.model';
 
 export class TasksPage{
@@ -8,10 +8,25 @@ export class TasksPage{
         this.page = page;
     }
 
+    async go() {
+        await this.page.goto("http://192.168.1.2:3000/");
+    }
+
     async create(task: TaskModel) {
         const inputTaskname = this.page.locator('input[class*=InputNewTask]');
         await inputTaskname.fill(task.name);
     
         await this.page.click('css=button >> text=Create');
+    }
+
+    async shouldHaveTask(taskName: string) {
+        const target = this.page.locator('css=.task-item p >> text=${taskName}');
+        await expect(target).toBeVisible();
+    
+    }
+
+    async alerthaveText(text: string) {
+     const target = this.page.locator('.swa12-html-container');
+     await expect(target).toHaveText(text);
     }
 }
