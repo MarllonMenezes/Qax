1a2d195bdc1cba2a8667743903dfdaeaff9e1132
Marllon Menezes <marllon_dallas@hotmail.com>
2023-05-13 18:41:56 -0300

    Atualização de customs

--- playwrith-Mark/tests/home.spec.ts
+++ playwrith-Mark/tests/home.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 
 test('web app deve estar online', async ({page}) => {
-  await  page.goto('http://192.168.1.3:3000/');
+  await  page.goto('http://localhost:3000/');
   await  expect(page).toHaveTitle('Gerencie suas tarefas com Mark L');
 });
